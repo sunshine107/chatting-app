8c54fecdfba9417b157add13641b80851d9497e7
alexchitu <alexchitu1991@outlook.com>
2018-06-28 23:38:15 +0300

    add

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { SignupFormComponent } from './app/signup-form/signup-form.component';
+import { LoginFormComponent } from './app/login-form/login-form.component';
+import { ChatroomComponent } from './app/chatroom/chatroom.component';
+
+//These are various pages of this project depeding on my our Url.
+export const appRoutes: Routes = [
+    { path: 'signup', component: SignupFormComponent },
+    { path: 'login', component: LoginFormComponent },
+    { path: 'chat', component: ChatroomComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full'},
+];
