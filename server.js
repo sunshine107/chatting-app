eed71aca1e2c2e3888e51f6e660b2a858f355327
alexchitu <alexchitu1991@outlook.com>
2018-06-29 00:20:28 +0300

    hi

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(express.static(path.join(__dirname, 'dist/base-chat-master'))); // Point static path to dist
+ 
+app.use('/api', api); // Set our api routes 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/base-chat-master/index.html'));
+}); //Catch all other routes and return the index file
+ 
+const port = process.env.PORT || '5000';  //port setting
+app.set('port', port);
+app.listen(port, ()=> console.log(`Listening at localhost:${port}`));
