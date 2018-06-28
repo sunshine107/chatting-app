77f34eda81e624fd15a3d0ee0be24c09a3c3be30
alexchitu <alexchitu1991@outlook.com>
2018-06-29 00:28:46 +0300

    hi

--- server.js
+++ server.js
@@ -25,11 +25,10 @@ app.use(function (req, res, next) {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); 
-app.use(express.static(path.join(__dirname, 'dist/base-chat-master'))); // Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist/'))); // Point static path to dist
  
-app.use('/api', api); // Set our api routes 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/base-chat-master/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 }); //Catch all other routes and return the index file
  
 const port = process.env.PORT || '5000';  //port setting
