ec424b0a26c201c80544425ddd96bdbaf011a81b
alexchitu <alexchitu1991@outlook.com>
2018-06-29 00:32:11 +0300

    hi

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { SignupFormComponent } from './app/signup-form/signup-form.component';
 import { LoginFormComponent } from './app/login-form/login-form.component';
 import { ChatroomComponent } from './app/chatroom/chatroom.component';
 
-//These are various pages of this project depeding on my our Url.
+// These are various pages of this project depeding on my our Url.
 export const appRoutes: Routes = [
     { path: 'signup', component: SignupFormComponent },
     { path: 'login', component: LoginFormComponent },
